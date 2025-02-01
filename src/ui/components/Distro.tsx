14e6b623eb05bb454a25f346c28b36a3c5662bd0
Nor <nordotdev@yahoo.com>
2025-02-02 01:10:28 +0200

    added hover effect for the distro cards

--- src/ui/components/Distro.tsx
+++ src/ui/components/Distro.tsx
@@ -12,7 +12,8 @@ interface Props {
 export const Distro: FC<Props> = (props: Props) => {
   return <button
     hx-get={props.link} hx-target="#content" hx-swap="outerHTML"
-    className="text-center p-5 lg:p-10 text-2xl lg:text-5xl border-4 border-zinc-700 border-dashed flex flex-col items-center justify-center gap-5 lg:gap-10 "
+    className="text-center p-5 lg:p-10 text-2xl lg:text-5xl border-4 
+    border-zinc-700 hover:border-green-900 border-dashed flex flex-col items-center justify-center gap-5 lg:gap-10 "
   >
     <img
       className="w-[150px] h-[150px] lg:w-[400px] lg:h-[400px]"
